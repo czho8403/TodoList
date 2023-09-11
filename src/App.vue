83141a73e01b08e465bb8acbf8e2f59e2dab911d
czho8403 <135591169+czho8403@users.noreply.github.com>
2023-09-11 06:18:40 +0000

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref } from 'vue'
+
+// give each todo a unique id
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+</script>
+
+<template>
+   <form @submit.prevent="addTodo" style="color:blue;font-size:28px;">
+    <input v-model="newTodo">
+    <button style="color:forestgreen;background-color:yellow;">Add Todo</button>    
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id" style="color:blue;font-size:28px;">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)"
+        style="color:red;background-color:lightpink;">X</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
